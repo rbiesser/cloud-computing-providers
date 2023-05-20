aa77136aacef4477cca669706566b8ee80439472
Robbie Biesser <rbiesser@gmail.com>
2023-05-20 15:30:37 -0400

    chore: install mapbox-gl

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN || ""
 
 function App() {
+
+const mapContainer = useRef<HTMLDivElement>(null);
+const map = useRef<mapboxgl.Map>();
+// TODO: can this be a single object?
+// eslint-disable-next-line
+const [lng, setLng] = useState(-96);
+// eslint-disable-next-line
+const [lat, setLat] = useState(37.8);
+// eslint-disable-next-line
+const [zoom, setZoom] = useState(3);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer?.current?mapContainer.current:"",
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
